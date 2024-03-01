15f591a423abda69e8ec9df36e9fa9ad12fe21e1
Ao Li <leeleo3x@gmail.com>
2024-03-01 17:45:29 -0500

    fix.

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -6,10 +6,17 @@ export default async function QueryProcessor(query: string): Promise<string> {
         apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
         baseURL: "https://app.qusic.me/openai/v1",
     });
-    const { data: completion, response } = await openai.completions.create({
-        prompt:  query +' Can you only include answers in your response.',
-        model: 'gpt-3.5-turbo-instruct',
-      }).withResponse();
-
-    return completion.choices[0].toString();
+    const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: query +' Can you only include answers in your response.' }],
+    model: "gpt-3.5-turbo",
+    });
+    // const { data: completion, response } = await openai.completions.create({
+    //     prompt:  query +' Can you only include answers in your response.',
+    //     model: 'gpt-3.5-turbo-instruct',
+    //   }).withResponse();
+    console.log(completion)
+    let obj = JSON.parse(completion);
+    console.log(obj)
+    // console.log(completion["choices"])
+    return obj["choices"][0]["message"]["content"];
 }
